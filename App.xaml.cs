d2cefce1304ebade0d9eb3331efddb7f2ceaa484
Marie <mmdevq@gmail.com>
2024-12-04 08:14:46 -0800

    ui

--- App.xaml.cs
+++ App.xaml.cs
@@ -10,9 +10,9 @@ namespace MauiWeatherApi
             MainPage = new WeatherInfoPage();
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+        //protected override Window CreateWindow(IActivationState? activationState)
+        //{
+        //    return new Window(new AppShell());
+        //}
     }
 }
