bb86f3635ddb2ee1a0e85df80c59120e0f7ac918
Marie <mmdevq@gmail.com>
2024-12-03 18:17:20 -0800

    weather pg

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace MauiWeatherApi
+﻿using MauiWeatherApi.Pages;
+
+namespace MauiWeatherApi
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            MainPage = new WeatherInfoPage();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
